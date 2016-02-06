f5aa0de24e30e94a3b461121728d8678d01e544d
cerkit <mearls@hotmail.com>
2016-02-05 21:40:28 -0600

    Removed main tags from templates. Cleaned up link-to-icon mapping code. Moved link-to-icon mapping code to its own script file.

--- ghost-cerkit-theme/assets/js/bundle.js
+++ ghost-cerkit-theme/assets/js/bundle.js
@@ -286,6 +286,80 @@ function setSelectedOption(selectedTheme) {
 "use strict";
 
 /************************************************************************************************
+* Navbar Icons
+* 
+* 
+* Here is a sample use:
+* 
+*  window.linkIconMap.defaultIconSize = 'fa-lg';
+*  window.addLinkIcon('nav-home', 'fa-home');
+*  window.addLinkIcon('nav-about', 'fa-user');
+*  window.addLinkIcon('nav-my-public-key', 'fa-key');
+*  window.addLinkIcon('nav-test', 'fa-cogs');
+* 
+*  Note: for size, the following values are valid:
+*  
+*  '' (will use default fontawesome size), 
+*  'fa-lg'
+*  'fa-2x'
+*  'fa-3x'
+*  'fa-4x'
+*  'fa-5x'
+* 
+*  If you do not supply a size for your icon, then the theme will use the default size 
+*  as determined by the defaultNavbarIconSize variable
+* 
+*  You can find a list of icons to use at http://fontawesome.io
+* 
+************************************************************************************************/
+window.linkIconMap = {};
+window.addLinkIcon = function (target, icon, size) {
+    // check to see if we have maps defined already
+    if (!window.linkIconMap.maps) {
+        // if not, define it
+        window.linkIconMap.maps = [];
+    }
+
+    // if we have a size passed in, use it, otherwise use the default icon size on our icon map. If that's missing, use nothing (Font Awesome default size)
+    var iconSize = size ? size : 'defaultIconSize' in window.linkIconMap ? window.linkIconMap.defaultIconSize : '';
+    window.linkIconMap.maps.push({ "target": target, "icon": icon, "size": iconSize });
+};
+
+var curIconMap;
+var curSize;
+/***********************************************************************************************/
+
+function bindLinkIcons() {
+    if (window.linkIconMap.maps) {
+        for (var i = 0; i < window.linkIconMap.maps.length; i++) {
+            // get a handle on the current icon map
+            curIconMap = linkIconMap.maps[i];
+
+            // set the icon on the navbar item
+            createIcon(curIconMap.target, curIconMap.icon, curIconMap.size);
+        }
+    } else {
+        console.warn('cerkit-bootstrap theme supports navbar link icons. Add the following to your footer in code injection: \<script\>window.addLinkIcon(/* target = */ "nav-home", /* icon = */ "fa-home", /* (optional) size = */ "fa-3x");\</script\>');
+    }
+}
+
+function createIcon(target, icon, size) {
+    var iconElement = $(document.createElement('i')).attr('class', 'fa fa-fw ' + icon + ' ' + size).append('&nbsp;');
+    var targetNavbarItem = $('.' + target);
+    var targetItemFirstChild = $(targetNavbarItem).children()[0];
+
+    // figure out if the nav item has any links in it. If so, use that as the icon parent.
+    // Otherwise, use the navbarIconItem.
+    var iconParentElement = targetItemFirstChild == null ? targetNavbarItem : targetItemFirstChild;
+
+    // insert the icon element at the beginning of the parent
+    $(iconParentElement).prepend(iconElement);
+}
+
+$(bindLinkIcons);
+"use strict";
+
+/************************************************************************************************
 * Pagination variables
 *************************************************************************************************
 * define the vars we'll use later 
@@ -314,89 +388,24 @@ var showThemeSelector = true;
 ************************************************************************************************/
 var subscriptionLink = null;
 
-/************************************************************************************************
-* Navbar Icons
-* 
-* Requires that a JSON object named "navbarIconMap" be defined in your FOOTER in Code Injection
-* 
-* Here is a sample JSON definition:
-* 
-*   var navbarIconMap = { 
-*        'defaultIconSize' : 'fa-lg', 
-*        'iconMaps' : [
-*			{ 'target' : 'nav-home', 'icon' : 'fa-home' },
-*			{ 'target' : 'nav-about', 'icon' : 'fa-user' },
-*			{ 'target' : 'nav-my-public-key', 'icon' : 'fa-key' }
-*		]
-*    };
-* 
-*  Note: for defaultIconSize, the following values are valid:
-*  
-*  '' (will use default fontawesome size), 
-*  'fa-lg'
-*  'fa-2x'
-*  'fa-3x'
-*  'fa-4x'
-*  'fa-5x'
-* 
-*  If you do not supply a size for your icon, then the theme will use the default size 
-*  as determined by the defaultNavbarIconSize variable
-* 
-*  You can find a list of icons to use at http://fontawesome.io
-* 
-************************************************************************************************/
-var curIconMap;
-var curSize;
-var targetNavbarItem;
-var targetItemFirstChild;
-var iconParentElement;
-/***********************************************************************************************/
-
-$(document).ready(function () {
-
+function moveSidebarItems() {
     // move items to the sidebar
     $('.sidebar-component').each(function () {
         $(this).detach().appendTo($('#sidebar-component-container'));
     });
-
-	// see if we need to change the link on the subscription button to an alternate...
-    if (subscriptionLink != null) {
-    	$('#subscribe-button').attr('href', subscriptionLink);
-
-    	// change the link for the site alternate, as well
-    	$('link [type*="application/rss+xml"]').attr('href', subscriptionLink);
-    }
-
-    if (navbarIconMap != null) {
-    	for (var i = 0; i < navbarIconMap.iconMaps.length; i++) {
-    		curIconMap = navbarIconMap.iconMaps[i];
-			// see if the current map provided a size. If not, use the default
-    		curSize = 'size' in curIconMap ? curIconMap.size : navbarIconMap.defaultIconSize;
-
-    		// set the icon on the navbar item
-    		console.log('curIconMap.target = ' + curIconMap.target);
-
-    		targetNavbarItem = $('.' + curIconMap.target);
-
-    		console.log('$(targetNavbarItem).html() = ' + $(targetNavbarItem).html());
-			
-    		// figure out if the nav item has any links in it. If so, use that as the icon parent.
-    		targetItemFirstChild = $(targetNavbarItem).children()[0];
-
-    	    // Otherwise, use the navbarIconItem.
-    		iconParentElement = targetItemFirstChild == null ? targetNavbarItem : targetItemFirstChild;
-
-    		console.log($(iconParentElement).html());
-
-    	    // insert the icon element at the beginning of the parent
-    		var iconElement = createIcon(curIconMap.icon, curSize);
-    		$(iconParentElement).prepend(iconElement);
-    	}
-    }
-});
-
-function createIcon(icon, size) {
-    var iconElement = $(document.createElement('i')).attr('class', 'fa fa-fw ' + icon + ' ' + size).append('&nbsp;');
-    console.log('iconElement class attribute value: "' + iconElement.attr('class') + '"');
-    return iconElement;
 }
+
+$(moveSidebarItems);
+
+
+function setAlternateSubscriptionLink() {
+    // see if we need to change the link on the subscription button to an alternate...
+    if (subscriptionLink != null) {
+        $('#subscribe-button').attr('href', subscriptionLink);
+
+        // change the link for the site alternate, as well
+        $('link [type*="application/rss+xml"]').attr('href', subscriptionLink);
+    }
+}
+
+$(setAlternateSubscriptionLink);
---
+++ ghost-cerkit-theme/assets/js/link-to-icon-mapper.js
@@ -0,0 +1,74 @@
+﻿"use strict";
+
+/************************************************************************************************
+* Navbar Icons
+* 
+* 
+* Here is a sample use:
+* 
+*  window.linkIconMap.defaultIconSize = 'fa-lg';
+*  window.addLinkIcon('nav-home', 'fa-home');
+*  window.addLinkIcon('nav-about', 'fa-user');
+*  window.addLinkIcon('nav-my-public-key', 'fa-key');
+*  window.addLinkIcon('nav-test', 'fa-cogs');
+* 
+*  Note: for size, the following values are valid:
+*  
+*  '' (will use default fontawesome size), 
+*  'fa-lg'
+*  'fa-2x'
+*  'fa-3x'
+*  'fa-4x'
+*  'fa-5x'
+* 
+*  If you do not supply a size for your icon, then the theme will use the default size 
+*  as determined by the defaultNavbarIconSize variable
+* 
+*  You can find a list of icons to use at http://fontawesome.io
+* 
+************************************************************************************************/
+window.linkIconMap = {};
+window.addLinkIcon = function (target, icon, size) {
+    // check to see if we have maps defined already
+    if (!window.linkIconMap.maps) {
+        // if not, define it
+        window.linkIconMap.maps = [];
+    }
+
+    // if we have a size passed in, use it, otherwise use the default icon size on our icon map. If that's missing, use nothing (Font Awesome default size)
+    var iconSize = size ? size : 'defaultIconSize' in window.linkIconMap ? window.linkIconMap.defaultIconSize : '';
+    window.linkIconMap.maps.push({ "target": target, "icon": icon, "size": iconSize });
+};
+
+var curIconMap;
+var curSize;
+/***********************************************************************************************/
+
+function bindLinkIcons() {
+    if (window.linkIconMap.maps) {
+        for (var i = 0; i < window.linkIconMap.maps.length; i++) {
+            // get a handle on the current icon map
+            curIconMap = linkIconMap.maps[i];
+
+            // set the icon on the navbar item
+            createIcon(curIconMap.target, curIconMap.icon, curIconMap.size);
+        }
+    } else {
+        console.warn('cerkit-bootstrap theme supports navbar link icons. Add the following to your footer in code injection: \<script\>window.addLinkIcon(/* target = */ "nav-home", /* icon = */ "fa-home", /* (optional) size = */ "fa-3x");\</script\>');
+    }
+}
+
+function createIcon(target, icon, size) {
+    var iconElement = $(document.createElement('i')).attr('class', 'fa fa-fw ' + icon + ' ' + size).append('&nbsp;');
+    var targetNavbarItem = $('.' + target);
+    var targetItemFirstChild = $(targetNavbarItem).children()[0];
+
+    // figure out if the nav item has any links in it. If so, use that as the icon parent.
+    // Otherwise, use the navbarIconItem.
+    var iconParentElement = targetItemFirstChild == null ? targetNavbarItem : targetItemFirstChild;
+
+    // insert the icon element at the beginning of the parent
+    $(iconParentElement).prepend(iconElement);
+}
+
+$(bindLinkIcons);
