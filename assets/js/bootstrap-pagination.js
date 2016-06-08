1e2784a82a8c9dc7386babd50c15db2c1b9089ee
cerkit <mearls@hotmail.com>
2016-06-08 14:59:34 -0500

    Removed Visual Studio files and configuration.

---
+++ assets/js/bootstrap-pagination.js
@@ -0,0 +1,108 @@
+/*********************************************************************************************************
+* bootstrap-pagination.js
+*
+* This script will generate a bootstrap compliant pagination component using Ghost pagination variables.
+* It requires a custom pagination.hbs file in your partials folder for your Ghost theme.
+*
+*********************************************************************************************************/
+
+$(document).ready(function() {	
+	//alert('prev: ' + prev + '\r\npages: ' + pages + '\r\npage: ' + page + '\r\nnext: ' + next);
+
+	// see if there are any previous pages
+	// if so, append them to the pagination ul
+	if (prev > 0) {
+		$('ul.bootstrap-pagination').append('<li class="prev"><a href="' + pageUrlFirst + '" title="Go to first page" aria-label="First"><span aria-hidden="true"><i class="fa fa-angle-double-left"></i></i></span></a></li>');
+		$('ul.bootstrap-pagination').append('<li class="prev"><a href="' + pageUrlPrev + '" title="Go to previous page" aria-label="Previous"><span aria-hidden="true"><i class="fa fa-angle-left"></i></span></a></li>');
+	}
+	
+	if (useSimplePagination) {
+		doSimplePagination();
+	}
+	else {
+		doComplexPagination();
+	}
+	
+	// if we have pages after this one, display the 'next' buttons
+	if (next > 0) {
+		$('.bootstrap-pagination').append('<li class="nxt"><a href="' + pageUrlNext + '" title="Go to next page" aria-label="Next"><span aria-hidden="true"><i class="fa fa-angle-right"></i></span></a></li>');
+		$('ul.bootstrap-pagination').append('<li class="nxt"><a href="' + pageUrlLast + '" title="Go to last page" aria-label="Last"><span aria-hidden="true"><i class="fa fa-angle-double-right"></i></i></span></a></li>');
+	}
+});
+
+// addPageLink() - Adds a <li> element containing a link to a given page. 
+// This will also add the appropriate screen reader labels.
+// pageNum - the number of the page to create a link to
+function addPageLink(pageNum) {
+	$('.bootstrap-pagination').append("<li class='page" + pageNum + "'><a href='" + pageUrl + "page/" + pageNum + "/' title='Go to page " + pageNum + "' aria-label='Go to page " + pageNum + "'>" + pageNum + "</a></li>");
+}
+
+function doSimplePagination() {
+	$('.bootstrap-pagination').append('<li><a href="' + pageUrl + 'page/"' + page + '" aria-label="Page ' + page + ' of ' + pages + '">Page ' + page + ' of ' + pages + '</a></li>');
+}
+
+function doComplexPagination() {
+	// if numbersSurroundingEllipses is < 0 (-1 is the preferred value for this use case), then we need to reset it to max pages so we'll show all pages
+	if (numbersSurroundingEllipses < 0) {
+		numbersSurroundingEllipses = pages;
+	}
+	else if (numbersSurroundingEllipses < 2) {
+		// make sure our numbers surrounding the ellipses isn't too small to function properly
+		numbersSurroundingEllipses = 2;
+	}
+	
+	var curPage = page;
+	var showEllipses = true;
+	// we need a number to see when to omit the ellipses and just show the last few page numbers
+	// this happens as we get close to the end
+	var maxAdjusted = pages - (numbersSurroundingEllipses * 2);
+	// if our current page falls within the last few page numbers, we need to omit the ellipses
+	// and show the "middle" page that would normally be covered up by the ellipses
+	if (curPage > maxAdjusted) {	
+		showEllipses = false;
+		// adjust because we're adding a page to the middle where the ellipses was
+		// we're showing the whole span of numbers until they click on a number below the threshold
+		curPage = pages - (numbersSurroundingEllipses * 2);
+	}
+	else{
+		// adjust so we show the page below the current page
+		curPage--;
+	}
+	
+	// check the floor...
+	if (curPage < 1) {
+		curPage = 1 ;
+	};
+	
+	// we need a variable to tell us when to stop adding pages and show the ellipses
+	var firstLiCount = curPage + numbersSurroundingEllipses;
+
+	
+	// create the list items for each page number
+	for (var i = curPage; i < firstLiCount ; i++) {
+		addPageLink(i);
+	}
+	
+	// show the ellipses (or not if we're near the end) for pages above our threshold
+	if (pages > numbersSurroundingEllipses) {
+		
+		if (showEllipses) {
+			//append the ellipses as a link that simply navigates to the current page
+			$('.bootstrap-pagination').append('<li><a href="' + pageUrl + 'page/"' + page + '">&#x2026;</a></li>');
+		}
+		else
+		{
+			// add the "center" page instead of the ellipses
+			// this happens when we get close to the end of the page numbers
+			addPageLink(pages - numbersSurroundingEllipses);
+		}
+		
+		// loop through our remaining page numbers and create the links
+		for(i = pages - numbersSurroundingEllipses + 1; i <= pages; i++){
+			addPageLink(i);
+		}
+	}		
+	
+	// set the current page link as active
+	$('li.page' + page).addClass('active');
+}
---
+++ assets/js/site-init.js
@@ -0,0 +1,74 @@
+"use strict";
+
+/************************************************************************************************
+* Pagination variables
+*************************************************************************************************
+* define the vars we'll use later 
+* (the pagination will set the values each time it appears on screen)
+************************************************************************************************/
+var prev;
+var pages;
+var page;
+var next;
+var pageUrl;
+var pageUrlPrev;
+var pageUrlNext;
+// define the number of page numbers that will be displayed before and after the ellipses
+var numbersSurroundingEllipses = 3; // override this in the Settings->Code Injection if desired
+var useSimplePagination = false;
+
+/************************************************************************************************
+* Theme selector variables
+************************************************************************************************/
+var linkToBootstrapCDN = "//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/";
+var themeStyleCss = "/bootstrap.min.css";
+var defaultTheme = 'cosmo';
+var showThemeSelector = true;
+
+function moveSidebarItems() {
+	// move items to the sidebar
+	$('.sidebar-component').each(function () {
+		$(this).detach().appendTo($('#sidebar-component-container'));
+	});
+}
+
+$(moveSidebarItems);
+
+
+function setAlternateSubscriptionLink() {
+	// see if we need to change the link on the subscription button to an alternate...
+	if (window.__themeCfg.alternateSubscribeLink != null) {
+		$('#subscribe-button').attr('href', window.__themeCfg.alternateSubscribeLink);
+
+		// change the link for the site alternate, as well
+		$('link [type*="application/rss+xml"]').attr('href', window.__themeCfg.alternateSubscribeLink);
+	}
+	else {
+		console.warn('ghost-cerkit-bootstrap theme supports an alternate RSS link (i.e. - FeedBurner) - put "\<script\>window.__themeCfg.alternateSubscribeLink = \'alternate rss url\';\<\/script\>" in Ghost code injection to turn it on');
+	}
+}
+
+$(setAlternateSubscriptionLink);
+
+function convertPrettyfierToPrism() {
+
+	if ($('pre.prettyprint').length) {
+
+
+		console.warn('there are ' + $('pre.prettyprint').length + ' prettyprint pre tags on this page');
+		console.warn('there are ' + $('pre.linenums').length + ' linenums pre tags on this page');
+
+		$('pre.prettyprint').each(function () {
+			$(this).removeClass('prettyprint').children('code').each(function () {
+				$(this).addClass('language-clike');
+			});
+		});
+
+		$('pre.linenums').each(function () {
+			$(this).removeClass('linenums').addClass('line-numbers');
+		});
+
+	}
+}
+
+$(convertPrettyfierToPrism);
