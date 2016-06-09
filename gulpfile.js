88137315125704b81bc5a19617b8db62bfefadcd
cerkit <mearls@hotmail.com>
2016-06-09 13:36:27 -0500

    added build and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var args = require('yargs').argv;
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var header = require('gulp-header');
@@ -7,14 +8,25 @@ var footer = require('gulp-footer');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 var cached = require('gulp-cached');
 var remember = require('gulp-remember');
 var rename = require('gulp-rename');
 var merge = require('merge2');
 var cleanCSS = require('gulp-clean-css');
-var sourcemaps = require('gulp-sourcemaps');
-var streamqueue  = require('streamqueue');
+var streamqueue = require('streamqueue');
 var zip = require('gulp-zip');
+var bump = require('gulp-bump');
+
+// deploy
+var $ = require('gulp-load-plugins')();
+var changelog = require('conventional-changelog');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var pkg = require('./package.json');
+var semver = require('semver');
+var util = require('gulp-util');
+var git = require('git');
 
 var DEST = 'src/assets/';
 var scriptSrc = 'dev/js/';
@@ -25,13 +37,15 @@ var cssGlob = 'dev/css/**/*.css';
 var buildTasks = ['styles', 'scripts'];
 
 gulp.task('styles', function () {
-    return merge(
-        gulp.src(cssGlob),
-        gulp.src(sassGlob)
-            .pipe(sass().on('error', sass.logError))
-    )
+  pkg = getPackageJson();
+
+  return merge(
+    gulp.src(cssGlob),
+    gulp.src(sassGlob)
+      .pipe(sass().on('error', sass.logError))
+  )
     .pipe(cached('styles'))
-    .pipe(remember('styles')) 
+    .pipe(remember('styles'))
     .pipe(concat('app.css'))
     .pipe(cleanCSS())
     .pipe(rename({ extname: '.min.css' }))
@@ -39,17 +53,18 @@ gulp.task('styles', function () {
 });
 
 gulp.task('scripts', function () {
-    // make sure the scripts get put into the correct order
-    return streamqueue({ objectMode: true },
-        gulp.src(scriptSrc + 'jquery.fitvids.js'),
-        gulp.src(scriptSrc + 'prism.js'),
-        gulp.src(scriptSrc + 'reading-time.js'),
-        gulp.src(scriptSrc + 'scroller.js'),
-        gulp.src(scriptSrc + 'cerkit-app.js'),
-        gulp.src(scriptSrc + 'startup.js')
-    )
+  // make sure the scripts get put into the correct order
+  return streamqueue({ objectMode: true },
+    gulp.src(scriptSrc + 'jquery.fitvids.js'),
+    gulp.src(scriptSrc + 'prism.js'),
+    gulp.src(scriptSrc + 'reading-time.js'),
+    gulp.src(scriptSrc + 'scroller.js'),
+    gulp.src(scriptSrc + 'cerkit-app.js'),
+    gulp.src(scriptSrc + 'startup.js')
+  )
     .pipe(cached('scripts'))        // only pass through changed files
     .pipe(jshint())                 // do special things to the changed files...
+    .pipe(jshint.reporter(stylish))
     .pipe(header('(function () {')) // e.g. jshinting ^^^
     .pipe(footer('})();'))          // and some kind of module wrapping
     .pipe(remember('scripts'))      // add back all files to the stream
@@ -61,27 +76,109 @@ gulp.task('scripts', function () {
 
 //Watch tasks
 gulp.task('watch', function () {
-    var scriptWatcher = gulp.watch(scriptsGlob, ['scripts']); // watch the same files in our scripts task
-    scriptWatcher.on('change', function (event) {
-        if (event.type === 'deleted') {                   // if a file is deleted, forget about it
-            delete cached.caches.scripts[event.path];       // gulp-cached remove api
-            remember.forget('scripts', event.path);         // gulp-remember remove api
-        }
-    });
+  var scriptWatcher = gulp.watch(scriptsGlob, ['scripts']); // watch the same files in our scripts task
+  scriptWatcher.on('change', function (event) {
+    if (event.type === 'deleted') {                   // if a file is deleted, forget about it
+      delete cached.caches.scripts[event.path];       // gulp-cached remove api
+      remember.forget('scripts', event.path);         // gulp-remember remove api
+    }
+  });
 
-    var styleWatcher = gulp.watch([sassGlob, cssGlob], ['styles']); // watch the same files in our scripts task
-    scriptWatcher.on('change', function (event) {
-        if (event.type === 'deleted') {                   // if a file is deleted, forget about it
-            delete cached.caches.styles[event.path];       // gulp-cached remove api
-            remember.forget('styles', event.path);         // gulp-remember remove api
-        }
-    });
+  var styleWatcher = gulp.watch([sassGlob, cssGlob], ['styles']); // watch the same files in our scripts task
+  scriptWatcher.on('change', function (event) {
+    if (event.type === 'deleted') {                   // if a file is deleted, forget about it
+      delete cached.caches.styles[event.path];       // gulp-cached remove api
+      remember.forget('styles', event.path);         // gulp-remember remove api
+    }
+  });
 });
 
-gulp.task('build', buildTasks, function(){
-    gulp.src('src/**/*.*')
-        .pipe(zip('ghost-cerkit-theme.zip'))
-        .pipe(gulp.dest('dist'));
-});
+var updateFileVersion = function () {
+  var versionArray = pkg.version.split('.');
+  var minorVersion = versionArray[versionArray.length - 1];
+
+};
 
 gulp.task('default', ['build']);
+
+var getPackageJson = function () {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+};
+
+/*****************************************************************************************
+ * Deploy
+ */
+
+var paths = gulp.paths;
+var tagTypes = {
+  patch: 'patch',
+  feature: 'minor',
+  release: 'major'
+};
+var version;
+
+
+gulp.task('deploy:init', buildTasks, function (done) {
+  var tagType = tagTypes[$.util.env.tag];
+  version = semver.inc(pkg.version, tagType);
+
+  if (!$.util.env.tag) return done('--tag is required');
+  if (!tagType) return done('--tag must be patch, feature or release');
+  if ($.util.env.f) return $.git.exec({ args: 'stash' });
+
+  exec('git status -s', function (err, stdout, stderr) {
+    if (err) return done(err);
+    if (stdout.length) return done('Repository does not have a clean status');
+    done();
+  });
+});
+
+gulp.task('deploy:zip', ['deploy:init'], function () {
+  var zipFilename = 'ghost-cerkit-theme-' + version + '.zip';
+  return gulp.src('src/**/*.*')
+    .pipe(zip(zipFilename))
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('changelog', ['deploy:zip'] , function (done) {
+  changelog({
+    repository: pkg.repository.url.replace('git://', 'https://')
+      .replace('.git', ''),
+    version: version,
+    file: 'CHANGELOG.md'
+  }, function (err, log) {
+    fs.writeFileSync(__dirname + './CHANGELOG.md', log);
+    done();
+  });
+});
+
+gulp.task('bump', ['changelog', 'deploy:zip'], function (done) {
+  return gulp.src(['./package.json'])
+    .pipe($.bump({ version: version }))
+    .pipe(gulp.dest('./'))
+    .pipe($.size({ title: '/', showFiles: true }));
+});
+
+
+gulp.task('deploy:commit', ['bump'], function () {
+  return gulp.src(['./package.json', './CHANGELOG.md'])
+    .pipe($.git.add())
+    .pipe($.git.commit('release: version ' + version))
+    .pipe($.size({ title: '/', showFiles: true }));
+});
+
+
+gulp.task('deploy:tag', ['deploy:commit'], function (done) {
+  $.git.tag('v' + version, 'release: version ' + version, done);
+});
+
+
+gulp.task('deploy:push', ['deploy:tag'], function (done) {
+  $.git.push('origin', 'master', { args: '--tags' }, done);
+});
+
+gulp.task('deploy', [
+  'deploy:init', 'changelog', 'deploy:zip', 'bump',
+  'deploy:commit', 'deploy:tag', 'deploy:push'
+]);
