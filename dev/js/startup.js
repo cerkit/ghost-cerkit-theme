7c72095858ea6efdaedf94996c31b1124e0eec01
Michael Earls <mearls@hotmail.com>
2016-06-23 11:50:23 -0500

    Fix pagination (#15)
    * removed the function surrounding the pagination script
    
    * Fixed pagination on the site

--- dev/js/startup.js
+++ dev/js/startup.js
@@ -1,7 +1,7 @@
 // startup initialization
 $(function () {
     "use strict";
-    
+
     _cerkit.methods.moveSidebarItems();
     _cerkit.config.setAlternateSubscriptionLink();
     _cerkit.config.convertPrettyfierToPrism();
@@ -31,20 +31,20 @@ $(function () {
     // see if there are any previous pages
     // if so, append them to the pagination ul
     if (_cerkit.pagination.prev > 0) {
-        $('ul.bootstrap-pagination').append('<li class="prev"><a href="' + pageUrlFirst + '" title="Go to first page" aria-label="First"><span aria-hidden="true"><i class="fa fa-angle-double-left"></i></i></span></a></li>');
-        $('ul.bootstrap-pagination').append('<li class="prev"><a href="' + pageUrlPrev + '" title="Go to previous page" aria-label="Previous"><span aria-hidden="true"><i class="fa fa-angle-left"></i></span></a></li>');
+        $('ul.bootstrap-pagination').append('<li class="prev"><a href="' + _cerkit.pagination.pageUrlFirst + '" title="Go to first page" aria-label="First"><span aria-hidden="true"><i class="fa fa-angle-double-left"></i></i></span></a></li>');
+        $('ul.bootstrap-pagination').append('<li class="prev"><a href="' + _cerkit.pagination.pageUrlPrev + '" title="Go to previous page" aria-label="Previous"><span aria-hidden="true"><i class="fa fa-angle-left"></i></span></a></li>');
     }
-    
+
     if (_cerkit.pagination.useSimplePagination) {
         _cerkit.pagination.doSimplePagination();
     }
     else {
         _cerkit.pagination.doComplexPagination();
     }
-    
+
     // if we have pages after this one, display the 'next' buttons
     if (_cerkit.pagination.next > 0) {
-        $('.bootstrap-pagination').append('<li class="nxt"><a href="' + pageUrlNext + '" title="Go to next page" aria-label="Next"><span aria-hidden="true"><i class="fa fa-angle-right"></i></span></a></li>');
-        $('ul.bootstrap-pagination').append('<li class="nxt"><a href="' + pageUrlLast + '" title="Go to last page" aria-label="Last"><span aria-hidden="true"><i class="fa fa-angle-double-right"></i></i></span></a></li>');
+        $('.bootstrap-pagination').append('<li class="nxt"><a href="' + _cerkit.pagination.pageUrlNext + '" title="Go to next page" aria-label="Next"><span aria-hidden="true"><i class="fa fa-angle-right"></i></span></a></li>');
+        $('ul.bootstrap-pagination').append('<li class="nxt"><a href="' + _cerkit.pagination.pageUrlLast + '" title="Go to last page" aria-label="Last"><span aria-hidden="true"><i class="fa fa-angle-double-right"></i></i></span></a></li>');
     }
 });
