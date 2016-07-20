67251b6f36b73258031c8b170b2501bffb90384d
Michael Earls <mearls@hotmail.com>
2016-07-20 13:15:41 -0500

    added SoundCloud widget color config

--- dev/js/cerkit-app.js
+++ dev/js/cerkit-app.js
@@ -282,8 +282,14 @@ _cerkit = {
         },
         setSoundCloudOptions: function () {
           if (window.__themeCfg.soundcloudUserId) {
+            var widgetColor = 'ff9900'; // default to Orange
+
+            if (window.__themeCfg.soundCloudWidgetColor) {
+              widgetColor = window.__themeCfg.soundCloudWidgetColor;
+            }
+
             $('#soundCloudContainer').show();
-            $('#soundCloudPanel').append('<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/' + window.__themeCfg.soundcloudUserId + '&amp;color=ff9900&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>');
+            $('#soundCloudPanel').append('<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/' + window.__themeCfg.soundcloudUserId + '&amp;color=' + widgetColor + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>');
           }
           else {
             $('#soundCloudContainer').hide();
