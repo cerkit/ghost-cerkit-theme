216da41e8ddacaa7bb6fe429f4e3f485fcebc0ab
Michael Earls <mearls@hotmail.com>
2017-01-18 08:09:09 -0600

    Added adsense code and re-formatted pages (removed tabs and replaced with 2 spaces instead of 4) (#48)

--- dev/js/scroller.js
+++ dev/js/scroller.js
@@ -4,48 +4,48 @@
 
 /* globals jQuery, document */
 (function ($, undefined) {
-    "use strict";
+  "use strict";
 
-    var $document = $(document);
+  var $document = $(document);
 
-    $document.ready(function () {
+  $document.ready(function () {
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+    var $postContent = $(".post-content");
+    $postContent.fitVids();
 
-        $(".scroll-down").arctic_scroll();
+    $(".scroll-down").arctic_scroll();
 
+  });
+
+  // Arctic Scroll by Paul Adam Davis
+  // https://github.com/PaulAdamDavis/Arctic-Scroll
+  $.fn.arctic_scroll = function (options) {
+
+    var defaults = {
+      elem: $(this),
+      speed: 500
+    },
+
+    allOptions = $.extend(defaults, options);
+
+    allOptions.elem.click(function (event) {
+      event.preventDefault();
+      var $this = $(this),
+          $htmlBody = $('html, body'),
+          offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+          position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+          toMove;
+
+      if (offset) {
+        toMove = parseInt(offset);
+        $htmlBody.stop(true, false).animate({ scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+      } else if (position) {
+        toMove = parseInt(position);
+        $htmlBody.stop(true, false).animate({ scrollTop: toMove }, allOptions.speed);
+      } else {
+        $htmlBody.stop(true, false).animate({ scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+      }
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
+  };
 })(jQuery);
